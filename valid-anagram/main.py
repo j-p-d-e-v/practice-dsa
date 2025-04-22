99bd47407d3ca3b69823eaff6dafb2b1eb9e2e6d
JP <jp@tiktoklive.com>
2025-04-23 01:22:00 +0800

    PHP version left for valid-anagram

--- valid-anagram/main.py
+++ valid-anagram/main.py
@@ -1,4 +1,9 @@
-def is_anagram(s: str, t: str) -> bool:
+import time
+
+
+def is_anagram_v1(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
     b = t
     for a in s:
         already_found = False
@@ -12,12 +17,78 @@ def is_anagram(s: str, t: str) -> bool:
     return len(b) == 0
 
 
+def is_anagram_v2(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+    a = {}
+    b = {}
+    for i in s:
+        if not i in a.keys():
+            a[i] = 0
+        a[i] += 1
+    for i in t:
+        if not i in b.keys():
+            b[i] = 0
+        b[i] += 1
+    for i in a:
+        if i in b.keys():
+            b[i] = abs(b[i] - a[i])
+        if i in a.keys():
+            a[i] = 0
+    return abs(sum(a.values())) == 0 and sum(b.values()) == 0
+
+
+def is_anagram_v3(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+    return sorted(s) == sorted(t)
+
+
+print("=============================================")
 s = "racecar"
 t = "carrace"
 
-assert is_anagram(s, t) == True
+start_ts = time.time_ns()
+assert is_anagram_v1(s, t) == True
+print("V1 Completed at: {}".format((time.time_ns() - start_ts)))
 
+start_ts = time.time_ns()
+assert is_anagram_v2(s, t) == True
+print("V2 Completed at: {}".format((time.time_ns() - start_ts)))
+
+start_ts = time.time_ns()
+assert is_anagram_v3(s, t) == True
+print("V3 Completed at: {}".format((time.time_ns() - start_ts)))
+
+
+print("=============================================")
 s = "jar"
 t = "jam"
 
-assert is_anagram(s, t) == False
+start_ts = time.time_ns()
+assert is_anagram_v1(s, t) == False
+print("V1 Completed at: {}".format((time.time_ns() - start_ts)))
+
+start_ts = time.time_ns()
+assert is_anagram_v2(s, t) == False
+print("V2 Completed at: {}".format((time.time_ns() - start_ts)))
+
+start_ts = time.time_ns()
+assert is_anagram_v3(s, t) == False
+print("V3 Completed at: {}".format((time.time_ns() - start_ts)))
+
+print("=============================================")
+s = "asdasdadasdas" * 1000
+t = "abcdefhghiasd" * 1000
+
+start_ts = time.time_ns()
+assert is_anagram_v1(s, t) == False
+print("V1 Completed at: {}".format((time.time_ns() - start_ts)))
+
+start_ts = time.time_ns()
+assert is_anagram_v2(s, t) == False
+print("V2 Completed at: {}".format((time.time_ns() - start_ts)))
+
+start_ts = time.time_ns()
+assert is_anagram_v3(s, t) == False
+print("V3 Completed at: {}".format((time.time_ns() - start_ts)))
--- valid-anagram/main.rs
+++ valid-anagram/main.rs
@@ -1,4 +1,36 @@
-fn is_anagram(s: String, t: String) -> bool {
+use std::collections::HashMap;
+use std::time::Instant;
+
+// V2 - Slower than V1 for smaller inputs
+// V2 - Faster than V1 for larger inputs
+// o(s + t)
+fn is_anagram_v2(s: String, t: String) -> bool {
+    if s.len() != t.len() {
+        return false;
+    }
+    let mut a: HashMap<char, isize> = HashMap::new();
+    let mut b: HashMap<char, isize> = HashMap::new();
+    for i in s.chars() {
+        a.entry(i).and_modify(|e| *e += 1).or_insert(1);
+    }
+    for i in t.chars() {
+        b.entry(i).and_modify(|e| *e += 1).or_insert(1);
+    }
+    for (s_k, s_v) in a.iter_mut() {
+        if let Some(t_v) = b.get_mut(s_k) {
+            *t_v -= *s_v;
+            *s_v = 0;
+        }
+    }
+    a.into_values().sum::<isize>() == 0 && b.into_values().sum::<isize>() == 0
+}
+
+// V1 - good for smaller inputs
+// V1 - O(n^2)
+fn is_anagram_v1(s: String, t: String) -> bool {
+    if s.len() != t.len() {
+        return false;
+    }
     let mut b: Vec<char> = t.chars().map(|v| v.to_owned()).collect();
     for a in s.chars() {
         let mut already_found: bool = false;
@@ -21,11 +53,77 @@ fn is_anagram(s: String, t: String) -> bool {
 fn main() {
     let s: String = String::from("racecar");
     let t: String = String::from("carrace");
+    let timer = Instant::now();
+    assert!(
+        is_anagram_v1(s.clone(), t.clone()) == true,
+        "Not an anagram"
+    );
+    println!("V1 Completed at: {}", timer.elapsed().as_nanos());
 
-    assert!(is_anagram(s, t) == true, "Not an anagram");
+    let timer = Instant::now();
+    assert!(
+        is_anagram_v2(s.clone(), t.clone()) == true,
+        "Not an anagram"
+    );
+    println!("V2 Completed at: {}", timer.elapsed().as_nanos());
 
     let s: String = String::from("jar");
     let t: String = String::from("jam");
+    println!("=================================================");
 
-    assert!(is_anagram(s, t) == false, "Its an anagram");
+    let timer = Instant::now();
+    assert!(
+        is_anagram_v1(s.clone(), t.clone()) == false,
+        "Its an anagram"
+    );
+    println!("V1 Completed at: {}", timer.elapsed().as_nanos());
+
+    let timer = Instant::now();
+    assert!(
+        is_anagram_v2(s.clone(), t.clone()) == false,
+        "Its an anagram"
+    );
+    println!("V2 Completed at: {}", timer.elapsed().as_nanos());
+
+    let s = "abcdefghij".repeat(1000);
+    let t = "jihgfedcba".repeat(1000);
+    println!("=================================================");
+    let timer = Instant::now();
+    assert!(
+        is_anagram_v1(s.clone(), t.clone()) == true,
+        "Its an anagram"
+    );
+    println!("V1 Completed at: {}", timer.elapsed().as_nanos());
+
+    let timer = Instant::now();
+    assert!(
+        is_anagram_v2(s.clone(), t.clone()) == true,
+        "Its an anagram"
+    );
+    println!("V2 Completed at: {}", timer.elapsed().as_nanos());
+
+    let s = "abbdefghij".repeat(10000);
+    let t = "xihgfedcba".repeat(10000);
+    println!("=================================================");
+    let timer = Instant::now();
+    assert!(
+        is_anagram_v1(s.clone(), t.clone()) == false,
+        "Its not an anagram"
+    );
+    println!(
+        "V1 Completed at: {}(ns) {}(ms)",
+        timer.elapsed().as_nanos(),
+        timer.elapsed().as_millis()
+    );
+
+    let timer = Instant::now();
+    assert!(
+        is_anagram_v2(s.clone(), t.clone()) == false,
+        "Its not an anagram"
+    );
+    println!(
+        "V2 Completed at: {}(ns) {}(ms)",
+        timer.elapsed().as_nanos(),
+        timer.elapsed().as_millis()
+    );
 }
